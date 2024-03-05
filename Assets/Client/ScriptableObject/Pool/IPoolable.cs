45234144baa96acb223a705e403628e376fd0ac0
Nastia <adruts@mail.ru>
2024-03-05 17:25:51 +0300

    https://github.com/anastyadru/GAMESpaceWorld.0.git
    откорректировала по правилам стиль кода

--- Assets/Client/ScriptableObject/Pool/IPoolable.cs
+++ Assets/Client/ScriptableObject/Pool/IPoolable.cs
@@ -1,8 +1,8 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public interface IPoolable
 {
