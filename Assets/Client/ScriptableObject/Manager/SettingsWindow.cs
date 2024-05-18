00b5391b57690bedc9bf903ae8caefc38df90c0f
Nastia <adruts@mail.ru>
2024-05-18 22:47:42 +0300

    https://github.com/anastyadru/GAMESpaceWorld.0.git
    Закомментировала часть кода, чтобы запустить игру

--- Assets/Client/ScriptableObject/Manager/SettingsWindow.cs
+++ Assets/Client/ScriptableObject/Manager/SettingsWindow.cs
@@ -1,19 +1,19 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class SettingsWindow : BaseWindow
-{
-    public override void OnOpened()
-    {
-        Debug.Log("Settings window opened");
-    }
+// public class SettingsWindow : BaseWindow
+// {
+    // public override void OnOpened()
+    // {
+        // Debug.Log("Settings window opened");
+    // }
 
-    public override void OnClosed()
-    {
-        Debug.Log("Settings window closed");
-    }
-}
+    // public override void OnClosed()
+    // {
+        // Debug.Log("Settings window closed");
+    // }
+// }
--- Assets/Client/ScriptableObject/Manager/SoundEffectsWindow.cs
+++ Assets/Client/ScriptableObject/Manager/SoundEffectsWindow.cs
@@ -1,19 +1,19 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class SoundEffectsWindow : BaseWindow
-{
-    public override void OnOpened()
-    {
-        Debug.Log("SoundEffects window opened");
-    }
+// public class SoundEffectsWindow : BaseWindow
+// {
+    // public override void OnOpened()
+    // {
+        // Debug.Log("SoundEffects window opened");
+    // }
 
-    public override void OnClosed()
-    {
-        Debug.Log("SoundEffects window closed");
-    }
-}
+    // public override void OnClosed()
+    // {
+        // Debug.Log("SoundEffects window closed");
+    // }
+// }
