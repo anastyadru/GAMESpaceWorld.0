ce0c2eefd5b02594a5ef29f9d8637693f1b356ef
Nastia <adruts@mail.ru>
2024-05-18 22:46:05 +0300

    https://github.com/anastyadru/GAMESpaceWorld.0.git
    Закомментировала часть кода, чтобы запустить игру

--- Assets/Client/ScriptableObject/Manager/PlayAgainWindow.cs
+++ Assets/Client/ScriptableObject/Manager/PlayAgainWindow.cs
@@ -1,19 +1,19 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class PlayAgainWindow : BaseWindow
-{
-    public override void OnOpened()
-    {
-        Debug.Log("PlayAgain window opened");
-    }
+// public class PlayAgainWindow : BaseWindow
+// {
+    // public override void OnOpened()
+    // {
+        // Debug.Log("PlayAgain window opened");
+    // }
 
-    public override void OnClosed()
-    {
-        Debug.Log("PlayAgain window closed");
-    }
-}
+    // public override void OnClosed()
+    // {
+        // Debug.Log("PlayAgain window closed");
+    // }
+// }
