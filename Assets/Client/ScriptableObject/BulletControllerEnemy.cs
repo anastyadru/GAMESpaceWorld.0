9c116ad3a802f71b45988d15fb6e1aca33446c0b
Nastia <adruts@mail.ru>
2024-05-20 21:07:26 +0300

    https://github.com/anastyadru/GAMESpaceWorld.0.git
    Раскомментировала часть кода, чтобы запустить игру

--- Assets/Client/ScriptableObject/BulletControllerEnemy.cs
+++ Assets/Client/ScriptableObject/BulletControllerEnemy.cs
@@ -1,42 +1,42 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class BulletControllerEnemy : MonoBehaviour // IPoolable
-// {
-    // public float speed = 100;
+public class BulletControllerEnemy : MonoBehaviour // IPoolable
+{
+    public float speed = 100;
     
-    // private ObjectPool bulletPool;
+    private ObjectPool bulletPool;
 
-    // private void Awake()
-    // {
-        // bulletPool = FindObjectOfType<ObjectPool>();
-    // }
+    private void Awake()
+    {
+        bulletPool = FindObjectOfType<ObjectPool>();
+    }
 
-    // private void OnTriggerEnter(Collider other)
-    // {
-        // if (other.CompareTag("Player"))
-        // {
-            // Player player = other.GetComponent<Player>();
-            // player.health -= 5;
-            // if (player.health <= 0)
-            // {
-                // player.OnRelease();
-                // bulletPool.Release(this);
-            // }
-        // }
-    // }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            player.health -= 5;
+            if (player.health <= 0)
+            {
+                player.OnRelease();
+                bulletPool.Release(this);
+            }
+        }
+    }
     
-    // public void Update()
-    // {
-        // transform.Translate(Vector3.back * speed * Time.deltaTime);
-    // }
+    public void Update()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
 
-    // public void OnRelease()
-    // {
-        // gameObject.SetActive(false);
-    // }
-// }
+    public void OnRelease()
+    {
+        gameObject.SetActive(false);
+    }
+}
