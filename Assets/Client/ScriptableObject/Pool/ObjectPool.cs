ed5911126e7fcf40899b78d2f8f4837e2c13b1bd
Nastia <adruts@mail.ru>
2024-05-21 19:34:14 +0300

    https://github.com/anastyadru/GAMESpaceWorld.0.git
    Раскомментировала код ObjectPool

--- Assets/Client/ScriptableObject/Pool/ObjectPool.cs
+++ Assets/Client/ScriptableObject/Pool/ObjectPool.cs
@@ -1,23 +1,23 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class ObjectPool : MonoBehaviour
-// {
-    // private Dictionary<MonoBehaviour, Queue<MonoBehaviour>> poolDictionary = new Dictionary<MonoBehaviour, Queue<MonoBehaviour>>();
+public class ObjectPool : MonoBehaviour
+{
+    private Dictionary<MonoBehaviour, Queue<MonoBehaviour>> poolDictionary = new Dictionary<MonoBehaviour, Queue<MonoBehaviour>>();
 
-    // public void Start()
-    // {
-        // PrePool<BulletControllerPlayer>(bulletPrefabPlayer, 20);
-        // PrePool<BulletControllerEnemy>(bulletPrefabEnemy, 10);
-        // PrePool<Enemy>(PrefabEnemy, 10);
-    // }
+    public void Start()
+    {
+        PrePool<BulletControllerPlayer>(bulletPrefabPlayer, 20);
+        PrePool<BulletControllerEnemy>(bulletPrefabEnemy, 10);
+        PrePool<Enemy>(PrefabEnemy, 10);
+    }
 
-    // public void PrePool<T>(T prefab, int count) where T : MonoBehaviour, IPoolable
-    // {
+    public void PrePool<T>(T prefab, int count) where T : MonoBehaviour, IPoolable
+    {
         // if (!poolDictionary.ContainsKey(prefab))
         // {
             // Queue<MonoBehaviour> objectPool = new Queue<MonoBehaviour>();
