9a35d65f97b04ace27e5dd559747bd12957ca8ba
Nastia <adruts@mail.ru>
2024-07-24 15:13:52 +0300

    https://github.com/anastyadru/GAMESpaceWorld.0.git
    Отредактировала код класса ObjectPool

--- Assets/Client/ScriptableObject/Pool/ObjectPool.cs
+++ Assets/Client/ScriptableObject/Pool/ObjectPool.cs
@@ -10,28 +10,30 @@ public class ObjectPool : MonoBehaviour
     public BulletControllerEnemy bulletPrefabEnemy;
     public Enemy PrefabEnemy;
     
-    private Dictionary<Type, Queue<IPoolable>> poolDictionary = new Dictionary<Type, Queue<IPoolable>>();
+    public Dictionary<Type, Queue<IPoolable>> playerPoolDictionary = new Dictionary<Type, Queue<IPoolable>>();
+    public Dictionary<Type, Queue<IPoolable>> enemyPoolDictionary = new Dictionary<Type, Queue<IPoolable>>();
 
     public void Start()
     {
-        PrePool(bulletPrefabPlayer, 200);
-        PrePool(bulletPrefabEnemy, 200);
-        PrePool(prefabEnemy, 50);
+        PrePool<BulletControllerPlayer>(bulletPrefabPlayer, 200, playerPoolDictionary);
+        PrePool<BulletControllerEnemy>(bulletPrefabEnemy, 200, enemyPoolDictionary);
+        PrePool<Enemy>(PrefabEnemy, 50, enemyPoolDictionary);
     }
 
-    private void PrePool<T>(T prefab, int count) where T : MonoBehaviour, IPoolable
+    public void PrePool<T>(T prefab, int count, Dictionary<Type, Queue<IPoolable>> poolDict) where T : MonoBehaviour, IPoolable
     {
         Type type = typeof(T);
-        if (!poolDictionary.ContainsKey(type))
+        if (!poolDict.ContainsKey(type))
         {
             Queue<IPoolable> objectPool = new Queue<IPoolable>();
             for (int i = 0; i < count; i++)
             {
-                T obj = Instantiate(prefab);
+                T obj = GameObject.Instantiate(prefab) as T;
                 obj.gameObject.SetActive(false);
                 objectPool.Enqueue(obj);
             }
-            poolDictionary.Add(type, objectPool);
+
+            poolDict.Add(type, objectPool);
         }
     }
 
