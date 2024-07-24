795ab936e95f8fb791996db3d15e64ab3d826da7
Nastia <adruts@mail.ru>
2024-07-24 12:08:08 +0300

    https://github.com/anastyadru/GAMESpaceWorld.0.git
    Отредактировала код класса Player

--- Assets/Client/ScriptableObject/Player.cs
+++ Assets/Client/ScriptableObject/Player.cs
@@ -1,8 +1,5 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,30 +8,46 @@ public class Player : MonoBehaviour
     public GameObject lazerShot;
     public Transform lazerGun;
     private float nextShotTime;
-    
     public int health = 100;
 
-    public void Update()
+    void Update()
+    {
+        MovePlayer();
+        HandleShooting();
+    }
+
+    private void MovePlayer()
     {
         float mouseX = Input.GetAxis("Mouse X");
-        Vector3 newPosition = transform.position + new Vector3(mouseX * speed * Time.deltaTime, 0, 0);
-        newPosition.x = Mathf.Clamp(newPosition.x, -300f, 210f);
-        transform.position = newPosition;
+        Vector3 newPosition = transform.position + new Vector3(mouseX * Speed * Time.deltaTime, 0, 0);
+        transform.position = new Vector3(Mathf.Clamp(newPosition.x, -300f, 210f), newPosition.y, newPosition.z);
+    }
+
+    private void HandleShooting()
+    {
         if (Input.GetButton("Fire1") && Time.time > nextShotTime)
         {
             string selectedShip = PlayerPrefs.GetString("SelectedShip");
             if (selectedShip.Contains("SpaceshipRed"))
             {
-                Instantiate(lazerShot, lazerGun.position, transform.rotation);
+                Shoot(1);
                 nextShotTime = Time.time + 0.1f;
             }
             else if (selectedShip.Contains("SpaceshipBlue"))
             {
-                Instantiate(lazerShot, lazerGun.position, transform.rotation);
-                Instantiate(lazerShot, lazerGun.position, Quaternion.Euler(0, -15, 0) * transform.rotation);
-                Instantiate(lazerShot, lazerGun.position, Quaternion.Euler(0, 15, 0) * transform.rotation);
+                Shoot(3);
                 nextShotTime = Time.time + 0.2f;
             }
+        }
+    }
+
+    private void Shoot(int shotCount)
+    {
+        for (int i = 0; i < shotCount; i++)
+        {
+            float angle = (i - 1) * 15; // -15, 0, +15 for blue ship
+            Quaternion rotation = Quaternion.Euler(0, angle, 0) * transform.rotation;
+            Instantiate(lazerShot, lazerGun.position, rotation);
         }
     }
     
